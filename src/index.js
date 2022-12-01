e1c1f593e7e7cd4b01a9560a1257f4e2b7a8759d
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-12-01 19:14:37 +0200

    v1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@
  * @param {Object} object
  * @returns {string[]}
  */
-export const keys = (object) => {};
+export const keys = (object) => {
+    let arr = [];
+    for (let key in object) {
+        arr.push(key);
+    }
+    return arr;
+}
+
 
 /**
  * Принимает объект, возвращает массив значений объекта. Также без использования
@@ -12,14 +19,27 @@ export const keys = (object) => {};
  * @param {Object} object
  * @returns {any[]}
  */
-export const values = (object) => {};
+export const values = (object) => {
+    let arr = [];
+    for (let value in object) {
+        arr.push(object[value]);
+    }
+    return arr;
+};
 
 /**
  * Принимаем объект, возвращаем энтри. Все также без методов, используем цикл
  * @param {Object} object
  * @returns {[string, any][]}
  */
-export const entries = (object) => {};
+export const entries = (object) => {
+    let arr = [];
+    for (let entries in object) {
+        arr.push(`[${entries}, ${object[entries]}]`);
+    }
+    return arr;
+
+};
 
 /**
  * Тут наоборот, мы должны сделать из энтри объект обратно) Можете погуглить
@@ -27,4 +47,23 @@ export const entries = (object) => {};
  * @param {[string, any][]} entries
  * @returns {Object}
  */
-export const fromEntries = (entries) => {};
+export const fromEntries = (entries) => {
+
+     return Object.fromEntries(entries)
+
+//плохо работает
+    // let arr = [];
+    // for (let ent in entries) {
+    //     arr.push(`[${ent}, ${entries[ent]}]`);
+    // }
+    // return {...arr};
+
+    //тоже плохо работает
+   //  let obj = {};
+   //  for (let ent in entries) {
+   //      obj[ent] = entries[ent]
+   //  }
+   //  return obj;
+
+}
+
